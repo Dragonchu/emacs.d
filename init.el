caa91909f61703e60b32fb4dcd58f40bfceb832f
Dragonchu <dr.dragonchu@gmail.com>
2024-01-01 22:16:44 +0800

    增加codespaces

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 ;; Load additional configuration files
 (load "~/.emacs.d/evil.el")
+(load "~/.emacs.d/codespaces.el")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
